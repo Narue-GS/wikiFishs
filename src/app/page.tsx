7dde21ef5cc7d85b2963ddb2bf1745050d7dd98d
Naruê Gonçalves Santos <naruegit@gmail.com>
2024-03-21 14:24:38 -0300

    refactor: move the state to the client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,14 @@
+
 import Image from "next/image";
 import sql from "../db"
+import FishList from "./components/fishList";
 
 export default async function Home() {
   const data = await sql`SELECT * FROM fish`
-
-  console.log(data);
-
-
+  
   return (
     <>
-      <h1 className="font-bold pl-4 text-3xl">Oba, bem vindo ao seu lugar de apreciação de peixes</h1>
-      <hr />
-
-      <div>
-        <h1>Peixes</h1>
-        {
-          data.map((i) => (
-            <div className="p-6" key={i.id}>
-              <span>{i.id} - {i.name} - {i.score}</span>
-              <p>{i.s_name}</p>
-            </div>
-          ))
-        }
-        
-        
-      </div>
+     <FishList list={data}/>
     </>
   );
 }
