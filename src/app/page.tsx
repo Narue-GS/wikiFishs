36d25678c4f053080725f49a4ee0f4fc72df0da5
Naruê Gonçalves Santos <naruegit@gmail.com>
2024-04-01 11:22:50 -0300

    fix: set the page to dynamic to avoid static outdated content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 import FishList from "./components/fishList"
 import { select } from "./service"
 
+export const dynamic = "force-dynamic" 
+
 export default async function Home() {
   const data = await select()
   
+
+
   return (
     <>
 
